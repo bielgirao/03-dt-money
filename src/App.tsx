d9d03f38ea0dfe7416ac104fb342c183e3e74950
bielgirao <bielgirao@hotmail.com>
2024-08-31 22:49:25 -0300

    feat: global styles config

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import { ThemeProvider } from 'styled-components';
+import {defaultTheme} from "./styles/themes/default.ts";
+
 export function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h1>DT Money</h1>
-    </>
+    </ThemeProvider>
   )
 }
