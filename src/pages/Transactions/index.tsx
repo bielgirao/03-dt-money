3ddd0dfa090a669eeee5815975f23b6910165a71
bielgirao <bielgirao@hotmail.com>
2024-09-04 22:55:11 -0300

    feat: add context data and formatters to transactions component

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,9 +1,15 @@
+import {useContext} from "react";
+import {TransactionsContext} from "../../contexts/TransactionsContext.tsx";
 import {Header} from "../../components/Header";
 import {Summary} from "../../components/Summary";
-import {PriceHighLight, TransactionContainer, TransactionsTable} from "./styles.ts";
 import {SearchForm} from "./components/SearchForm";
+import {PriceHighLight, TransactionContainer, TransactionsTable} from "./styles.ts";
+import {dateFormatter, priceFormatter} from "../../utils/formatter.ts";
+
 
 export function Transactions() {
+    const {transactions} = useContext(TransactionsContext);
+
     return (
         <div>
             <Header />
@@ -13,46 +19,21 @@ export function Transactions() {
                 <SearchForm />
                 <TransactionsTable>
                     <tbody>
-                    <tr>
-                        <td width="50%">Website development</td>
-                        <td>
-                            <PriceHighLight variant="income">
-                                $12,000.00
-                            </PriceHighLight>
-                        </td>
-                        <td>Sale</td>
-                        <td>04/13/2024</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Groceries</td>
-                        <td>
-                            <PriceHighLight variant="expense">
-                                - $12,000.00
-                            </PriceHighLight>
-                        </td>
-                        <td>Shopping</td>
-                        <td>04/13/2024</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Family Dinner</td>
-                        <td>
-                            <PriceHighLight variant="expense">
-                                - $5,000.00
-                            </PriceHighLight>
-                        </td>
-                        <td>Food</td>
-                        <td>04/13/2024</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Monthly Salary</td>
-                        <td>
-                            <PriceHighLight variant="income">
-                                $19,000.00
-                            </PriceHighLight>
-                        </td>
-                        <td>Salary</td>
-                        <td>04/13/2024</td>
-                    </tr>
+                        {transactions.map(transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <PriceHighLight variant={transaction.type}>
+                                            {transaction.type === "expense" && '- '}
+                                            {priceFormatter.format(transaction.amount)}
+                                        </PriceHighLight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+                                </tr>
+                            )
+                        })}
                     </tbody>
                 </TransactionsTable>
             </TransactionContainer>
