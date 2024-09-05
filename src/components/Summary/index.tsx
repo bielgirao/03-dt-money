0ba6da622f6d13832dc8c7b94e1aa65e0c6eb98a
bielgirao <bielgirao@hotmail.com>
2024-09-04 22:54:45 -0300

    feat: add useSummary hook for summary logic

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,29 +1,34 @@
 import {SummaryContainer, SummaryCard} from "./styles.ts";
 import {ArrowCircleDown, ArrowCircleUp, CurrencyDollar} from 'phosphor-react';
 
+import {priceFormatter} from "../../utils/formatter.ts";
+import {useSummary} from "../../hooks/useSummary.ts";
+
 export function Summary() {
+    const summary = useSummary();
+
     return (
         <SummaryContainer>
             <SummaryCard>
                 <header>
-                    <span>Incoming</span>
+                    <span>Incomes</span>
                     <ArrowCircleUp size={32} color="#00b37e"/>
                 </header>
-                <strong>$17,400.00</strong>
+                <strong>{priceFormatter.format(summary.incomes)}</strong>
             </SummaryCard>
             <SummaryCard>
                 <header>
                     <span>Expenses</span>
                     <ArrowCircleDown size={32} color="#f75a68"/>
                 </header>
-                <strong>$17,400.00</strong>
+                <strong>{priceFormatter.format(summary.expenses)}</strong>
             </SummaryCard>
             <SummaryCard variant='green' >
                 <header>
                     <span>Balance</span>
                     <CurrencyDollar size={32} color="#fff"/>
                 </header>
-                <strong>$17,400.00</strong>
+                <strong>{priceFormatter.format(summary.total)}</strong>
             </SummaryCard>
         </SummaryContainer>
     );
