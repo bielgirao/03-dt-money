052a4c7bf6e518de5e4dc0d41e242bbc783c1323
bielgirao <bielgirao@hotmail.com>
2024-09-04 22:53:19 -0300

    feat: add transactions context

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import { ThemeProvider } from 'styled-components';
 import {defaultTheme} from "./styles/themes/default.ts";
 import {Transactions} from "./pages/Transactions";
 import {GlobalStyles} from "./styles/global.ts";
+import {TransactionsProvider} from "./contexts/TransactionsContext.tsx";
 
 export function App() {
 
   return (
     <ThemeProvider theme={defaultTheme}>
-        <Transactions />
+        <TransactionsProvider>
+            <Transactions />
+        </TransactionsProvider>
         <GlobalStyles />
     </ThemeProvider>
   )
