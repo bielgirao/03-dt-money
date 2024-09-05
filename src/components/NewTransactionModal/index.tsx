2a71c60687b5cc4736491798278f278f91e1c85e
bielgirao <bielgirao@hotmail.com>
2024-09-04 22:55:48 -0300

    feat: add react hook form and zod to newTransactionModal  and search forms

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,37 @@
 import * as Dialog from "@radix-ui/react-dialog";
 import {Overlay, Content, CloseButton, TransactionType, TransactionTypeButton} from "./styles.ts";
 import {ArrowCircleDown, ArrowCircleUp, X} from "phosphor-react";
+import * as z from 'zod';
+import {Controller, useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+const newTransactionFormSchema = z.object({
+    description: z.string(),
+    amount: z.number(),
+    category: z.string(),
+    type: z.enum(['income','expense'])
+});
+
+type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
+    const {
+        control,
+        register,
+        handleSubmit,
+        formState: {
+            isSubmitting,
+            isValid
+        }
+    } = useForm<NewTransactionFormInputs>({
+        resolver: zodResolver(newTransactionFormSchema)
+    })
+
+    async function handleCreateNewTransaction(data: NewTransactionFormInputs) {
+        await new Promise(resolve => setTimeout(resolve, 2000))
+        console.log(data)
+    }
+
     return (
         <Dialog.Portal>
             <Overlay />
@@ -12,23 +41,54 @@ export function NewTransactionModal() {
                     <X size={24} />
                 </CloseButton>
 
-                <form action="">
-                    <input type="text" placeholder="Description" required />
-                    <input type="number" placeholder="Amount" required />
-                    <input type="text" placeholder="Category" required />
+                <form onSubmit={handleSubmit(handleCreateNewTransaction)}>
+                    <input
+                        type="text"
+                        placeholder="Description"
+                        required
+                        {...register("description")}
+                    />
+                    <input
+                        type="number"
+                        placeholder="Amount"
+                        required
+                        {...register("amount", { valueAsNumber: true })}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Category"
+                        required
+                        {...register("category")}
+                    />
 
-                    <TransactionType>
-                        <TransactionTypeButton variant='income' value='income'>
-                            <ArrowCircleUp size={24} />
-                            Income
-                        </TransactionTypeButton>
-                        <TransactionTypeButton variant='expense' value='expense'>
-                            <ArrowCircleDown size={24} />
-                            Expense
-                        </TransactionTypeButton>
-                    </TransactionType>
+                    <Controller
+                        control={control}
+                        name="type"
+                        render={({field}) => {
+                            return (
+                                <TransactionType
+                                    onValueChange={field.onChange}
+                                    value={field.value}
+                                >
+                                    <TransactionTypeButton variant='income' value='income'>
+                                        <ArrowCircleUp size={24} />
+                                        Income
+                                    </TransactionTypeButton>
+                                    <TransactionTypeButton variant='expense' value='expense'>
+                                        <ArrowCircleDown size={24} />
+                                        Expense
+                                    </TransactionTypeButton>
+                                </TransactionType>
+                            )
+                        }}
+                    />
 
-                    <button type="submit">Add Transaction</button>
+                    <button
+                        type="submit"
+                        disabled={isSubmitting || !isValid}
+                    >
+                        Add Transaction
+                    </button>
                 </form>
             </Content>
         </Dialog.Portal>
